88203de67bb9d1ecbde0a324de17a03e4a8da86a
ptduy14 <phantanduy14@gmail.com>
2024-05-16 23:24:02 +0700

    add close button modal mobile screen

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var toggleButton = document.querySelector('.toggle');
 var modal = document.querySelector('.modal')
 var layer = document.querySelector('.layer')
+var closeModal = document.querySelector('.close_modal');
 
 toggleButton.addEventListener('click', () => {
     modal.classList.add("modal-active")
@@ -11,3 +12,10 @@ layer.addEventListener('click', () => {
     modal.classList.remove("modal-active")
     layer.classList.remove("layer-active")
 })
+
+if (closeModal) {
+    closeModal.addEventListener('click', () => {
+        modal.classList.remove("modal-active")
+        layer.classList.remove("layer-active")
+    })
+}
